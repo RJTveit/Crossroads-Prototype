1f2d9b69517d86b3b8bf9677ea479776bc8a07b2
Roscoe T <roscoe.tveit@gmail.com>
2020-10-18 16:34:53 -0700

    Added a new layout for the homepage, and started work on the artists layout page. Connecting new models and controllers is on-going.

---
+++ Crossroads/Controllers/ArtistsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Crossroads.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly ILogger<ArtistsController> _logger;
+
+        public ArtistsController(ILogger<ArtistsController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
